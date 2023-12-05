3fef9a69a9897776defd773efc59a5b1905186b3
Ernest Kwashie <ernestkwashie3@gmail.com>
2023-12-06 00:29:42 +0100

    deleted sqs.tf

--- sqs.tf
+++
@@ -1,9 +0,0 @@
-#SQS Resource block for SQS Queue.
-resource "aws_sqs_queue" "test-sqs" {
-  name                       = "lambda-sqs-queue"
-  delay_seconds              = var.sqs_delayseconds 
-  max_message_size           = var.max_message_size 
-  message_retention_seconds  = var.message_retention_seconds 
-  visibility_timeout_seconds = var.visibility_timeout_seconds 
-  receive_wait_time_seconds  = var.receive_wait_time_seconds
-}
