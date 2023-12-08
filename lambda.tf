e6539b8a4e435267fd3f113d1348396e9a7fb0c7
Lateef Taiwo <taiwolateef55@gmail.com>
2023-12-08 02:47:18 +0100

    modified lambda.tf and added images

--- lambda.tf
+++ lambda.tf
@@ -6,7 +6,7 @@ resource "aws_lambda_function" "wild_rides_lambda" {
   role          = aws_iam_role.lambda_iam_role.arn
   memory_size   = 128
   timeout       = 60
-  s3_bucket     = "wild-rides-bucket-0056"
+  s3_bucket     = aws_s3_bucket.s3-bucket.bucket
   s3_key        = "lambda-function.zip"
   depends_on    = [aws_iam_role.lambda_iam_role, aws_s3_bucket.s3-bucket, aws_s3_object.lambda-function-zip]
 
@@ -17,37 +17,37 @@ resource "aws_lambda_function" "wild_rides_lambda" {
   }
 }
 
-# Invoke Lambda Function
-# resource "aws_lambda_invocation" "invoke_test_event" {
-#   function_name = aws_lambda_function.wild_rides_lambda.function_name
-#   input         = <<EOT
-#  {
-#     "path": "/ride",
-#     "httpMethod": "POST",
-#     "headers": {
-#         "Accept": "*/*",
-#         "Authorization": "eyJraWQiOiJLTzRVMWZs",
-#         "content-type": "application/json; charset=UTF-8"
-#     },
-#     "queryStringParameters": null,
-#     "pathParameters": null,
-#     "requestContext": {
-#         "authorizer": {
-#             "claims": {
-#                 "cognito:username": "the_username"
-#             }
-#         }
-#     },
-#     "body": "{\"PickupLocation\":{\"Latitude\":47.6174755835663,\"Longitude\":-122.28837066650185}}"
-# }
+#Invoke Lambda Function
+resource "aws_lambda_invocation" "invoke_test_event" {
+  function_name = aws_lambda_function.wild_rides_lambda.function_name
+  input         = <<EOT
+ {
+    "path": "/ride",
+    "httpMethod": "POST",
+    "headers": {
+        "Accept": "*/*",
+        "Authorization": "eyJraWQiOiJLTzRVMWZs",
+        "content-type": "application/json; charset=UTF-8"
+    },
+    "queryStringParameters": null,
+    "pathParameters": null,
+    "requestContext": {
+        "authorizer": {
+            "claims": {
+                "cognito:username": "the_username"
+            }
+        }
+    },
+    "body": "{\"PickupLocation\":{\"Latitude\":47.6174755835663,\"Longitude\":-122.28837066650185}}"
+}
 
-#   EOT
+  EOT
 
-#   depends_on = [aws_lambda_function.wild_rides_lambda]
-# }
+  depends_on = [aws_lambda_function.wild_rides_lambda]
+}
 
-# Capture Lambda output
-#output "lambda_output" {
-#  value = aws_lambda_invocation.invoke_test_event.result
-#}
+#Capture Lambda output
+output "lambda_output" {
+ value = aws_lambda_invocation.invoke_test_event.result
+}
 
