3cb18b72b1677ec950f3fc58d9c8d1cebc93eb0c
Lateef Taiwo <taiwolateef55@gmail.com>
2023-12-11 02:06:17 +0100

    modified files

--- s3.tf
+++ s3.tf
@@ -22,14 +22,14 @@ resource "aws_s3_bucket_public_access_block" "s3-bucket-block" {
 data "archive_file" "source" {
   type        = "zip"
   source_dir  = "../Wild-Rydes/"
-  output_path = "../Wild-Rydes/index2.zip"
+  output_path = "../Wild-Rydes/index.zip"
 }
 
 # Upload the zip file to S3 bucket
 resource "aws_s3_object" "lambda-function-code" {
   bucket = aws_s3_bucket.s3-bucket.bucket
   source = data.archive_file.source.output_path
-  key    = "index2.zip"
+  key    = "index.zip"
   acl    = "private"
   depends_on = [aws_s3_bucket.s3-bucket]
 }
