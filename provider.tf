4a08b9faf254f1ba586804befbcb3d057d004608
Lateef Taiwo <taiwolateef55@gmail.com>
2023-12-23 00:07:38 +0100

    modified provider.tf

---
+++ provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.20.0"
+    }
+    external = {
+      source  = "hashicorp/external"
+      version = ">= 1.2.0"
+    }
+  }
+
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}    
+
+provider "external" {}
